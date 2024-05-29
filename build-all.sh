403bf52fb8d9329f58f20910097be79fa055e99f
ToasterUwU <Aki@ToasterUwU.com>
2024-05-29 21:08:20 +0200

    added build script, taks, workflow, and gitignore to ignore results of building

---
+++ build-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for dir in ./*; do
+    if [ -d "$dir" ]; then
+        for file in "$dir"/*.scad; do
+            echo "$file"
+            openscad --export-format binstl -o "${file%.scad}.stl" "$file"
+            openscad --render --autocenter --viewall "$file" -o "$dir/preview.png"
+        done
+    fi
+done
