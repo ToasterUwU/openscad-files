36367e491725f34361af955d5e4f717f7cd1f713
ToasterUwU <Aki@ToasterUwU.com>
2024-05-29 21:20:43 +0200

    use argument instead of alias

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+OPENSCAD_COMMAND=${1:-openscad}
+
 set -e
 
 for dir in ./*; do
     if [ -d "$dir" ]; then
         for file in "$dir"/*.scad; do
             echo "$file"
-            openscad --export-format binstl -o "${file%.scad}.stl" "$file"
-            openscad --render --autocenter --viewall "$file" -o "$dir/preview.png"
+            $OPENSCAD_COMMAND --export-format binstl -o "${file%.scad}.stl" "$file"
+            $OPENSCAD_COMMAND --render --autocenter --viewall "$file" -o "$dir/preview.png"
         done
     fi
 done
