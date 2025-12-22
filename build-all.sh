c805e1b43bf22aa22be736b2c17b9024ba9bb23b
ToasterUwU <Aki@ToasterUwU.com>
2025-12-22 10:42:38 +0100

    build in parallel since CPU usage is low, but it takes a long time

--- build-all.sh
+++ build-all.sh
@@ -8,8 +8,10 @@ for dir in ./*; do
     if [ -d "$dir" ]; then
         for file in "$dir"/*.scad; do
             echo "$file"
-            $OPENSCAD_COMMAND --export-format binstl -o "${file%.scad}.stl" "$file"
-            $OPENSCAD_COMMAND --render --autocenter --viewall "$file" -o "$dir/preview.png"
+            $OPENSCAD_COMMAND --export-format binstl -o "${file%.scad}.stl" "$file" &
+            $OPENSCAD_COMMAND --render --autocenter --viewall "$file" -o "$dir/preview.png" &
         done
     fi
 done
+
+wait
